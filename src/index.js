05c51e68e96aaafcb9e5da7e0312ae6ac6ee5964
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-05-09 13:03:31 -0500

    add gitignore

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import app from './app.js'
 import { connectDB } from './db.js'
 
 connectDB()
-app.listen(3000)
-console.log('Servidor corriendo en el puerto http://localhost:3000')
+const puerto = process.env.PORT || 3000
+const server = app.listen(puerto)
+console.log(`Servidor corriendo en el puerto ${puerto}`)
