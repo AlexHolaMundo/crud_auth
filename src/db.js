4a53d9d884945e7c013bd39051737cd07487a712
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-05-09 14:27:52 -0500

    variables

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
+import mongoose, { Mongoose } from 'mongoose'
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/userCrud')
+    const conexion = await mongoose.connect(process.env.conexion)
     console.log('>>> Conexión a la base de datos establecida')
   } catch (err) {
     console.log('Error al conectar a la base de datos', err)
