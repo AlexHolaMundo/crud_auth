70ba70b8621183bbabbb972142b30f665c0fccec
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-05-09 19:13:07 -0500

    update conexion

--- src/db.js
+++ src/db.js
@@ -1,10 +1,25 @@
-import mongoose, { Mongoose } from 'mongoose'
+import mongoose from 'mongoose'
 
 export const connectDB = async () => {
   try {
-    const conexion = await mongoose.connect(process.env.conexion)
+    const uri = process.env.conexion
+
+    if (!uri) {
+      throw new Error(
+        'La URL de conexión no está definida en process.env.conexion'
+      )
+    }
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+
+    const conexion = await mongoose.connect(uri, options)
     console.log('>>> Conexión a la base de datos establecida')
-  } catch (err) {
-    console.log('Error al conectar a la base de datos', err)
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message)
   }
 }
